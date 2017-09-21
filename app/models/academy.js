ec453bf64fa345c49c7c5c362cb8d7677e933fd0
Thomas Allen <tom@telega.org>
2017-09-21 15:18:06 +0100

    glossary styles and user pass update

--- app/models/academy.js
+++ app/models/academy.js
@@ -19,6 +19,10 @@ var AcademySchema = new Schema({
 	academyHomeCta: {
 		type:String,
 		default: ''
+	},
+	academyTOS: {
+		type: String,
+		default: 'This website (including any enclosures and attachments) has been produced for your exclusive use, for educational purposes only. Patsnap (UK) Limited, its employees, partners or agents do not accept any liability if this website is used for an alternative purpose from which it is intended, nor to any third party in respect of this website. All information on this website is provided strictly on an "as is" basis, with no guarantee of completeness, accuracy, timeliness or of the results obtained from the use of the information, and without warranty of any kind, express or implied, including, but not limited to warranties of performance, merchantability and fitness for a particular purpose. Under no circumstances, will Patsnap (UK) Limited, its employees, partners, or agents, be liable to you or any third party for any decision made or action taken in reliance on the information in this report or for any consequential, special or similar damages, even if advised of the possibility of such damages. Unless Patsnap (UK) Limited provides express prior written consent, no part of this website should be reproduced, distributed or communicated to any third party.'
 	}
 });
 
